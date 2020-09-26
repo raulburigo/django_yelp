5ee6de4cc17691671328708751fcd7108b4e3ca6
Raul Burigo <raul.burigo@gmail.com>
2020-09-26 13:43:14 -0300

    Procfile and adjustments

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django_yelp_clone.wsgi
--- django_yelp_clone/settings.py
+++ django_yelp_clone/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,14 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o_%c+25spw^jb=-tylrma0bptky#)s^zw-a2sj!)jfbk4+38!&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = [
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
